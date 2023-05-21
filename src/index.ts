9a117b05a2c9718c94e558cbe89abb6f15e3f183
Oleg <66309663+Insid1@users.noreply.github.com>
2023-05-21 11:14:42 +0700

    feat: adds typescript

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, {Request, Response, NextFunction} from "express"
 import {router as userRouter} from "./user/router.js";
 
 const PORT = 7070
@@ -13,23 +13,15 @@ app.use((req, res, next) => {
 app.use("/user", userRouter)
 
 app.get("/hello", (req, res) => {
-  // res.sendStatus(201)
-  throw new Error("Ошибга")
+  res.sendStatus(201)
+  // throw new Error("Ошибга")
 })
 
-app.use((err, req, res, next) => {
-  console.log(err instanceof Error)
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500)
-  if (err instanceof Error) {
-    res.json(err.message)
-  } else {
-    res.json("Unexpected error occurred!")
-  }
-  // console.log(err.message)
-  // next();
-  // res.se
+  res.json(err.message)
 })
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}. GO to http://localhost:${PORT}`)
 })
