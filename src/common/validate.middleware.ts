54c6c714aa9ab5e80c2fca2ad2c730ab05e07ab1
Oleg <66309663+Insid1@users.noreply.github.com>
2023-05-26 09:54:09 +0700

    fix: base controller context for middleware

--- src/common/validate.middleware.ts
+++ src/common/validate.middleware.ts
@@ -1,26 +1,18 @@
-import { IMiddleware } from "./middleware.interface";
-import { NextFunction, Request, Response } from "express";
 import { ClassConstructor, plainToInstance } from "class-transformer";
+import { NextFunction, Request, Response } from "express";
+import { IMiddleware } from "./middleware.interface";
 import { validate } from "class-validator";
 
 export class ValidateMiddleware implements IMiddleware {
 	constructor(private classToValidate: ClassConstructor<object>) {}
 
-	execute({ body }: Request, res: Response, next: NextFunction): void {
-		console.log("body", body);
+	async execute({ body }: Request, res: Response, next: NextFunction): Promise<void> {
 		const instance = plainToInstance(this.classToValidate, body);
-		console.log("instance", instance);
-		validate(instance)
-			.then((errors) => {
-				console.log("Errors", errors);
-				if (errors.length > 0) {
-					res.status(422).send(errors);
-				} else {
-					next();
-				}
-			})
-			.catch((e) => {
-				console.log("ERROR", e);
-			});
+		const errors = await validate(instance);
+		if (errors.length > 0) {
+			res.status(422).send(errors);
+		} else {
+			next();
+		}
 	}
 }
